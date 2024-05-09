44d78e7b8dfe66a823113faf751dce1e0a3319da
Gabriel <gabrielsiano2006@gmail.com>
2024-05-09 08:07:32 +0400

    Test push token

--- src/main/java/it/itismeucci/Griglia.java
+++ src/main/java/it/itismeucci/Griglia.java
@@ -9,8 +9,7 @@ public class Griglia {
     private static final char GIOCATORE1 = 'O';
     private static final char GIOCATORE2 = 'X';
 
-    private boolean turnoGiocatore1 = true; //true = giocatore1 - false = giocatore2
-
+    private boolean turnoGiocatore1 = true; // true = giocatore1 - false = giocatore2
 
     public Griglia() {
         this.grigliaPrincipale = new char[grandezzaGriglia][grandezzaGriglia];
@@ -22,7 +21,6 @@ public class Griglia {
         }
     }
 
-    
     public boolean segnaPunto(int riga, int colonna) {
         if (grigliaPrincipale[riga][colonna] == VUOTO) {
             if (turnoGiocatore1) {
@@ -31,52 +29,53 @@ public class Griglia {
                 grigliaPrincipale[riga][colonna] = GIOCATORE2;
             }
             turnoGiocatore1 = !turnoGiocatore1;
-            return true; //punto segnato
+            return true; // punto segnato
         }
-        return false; //la cella è già occupata
+        return false; // la cella è già occupata
     }
 
-
-    public char verificaVincitore() {  //controlla tutta la griglia per capire chi è il vincitore
+    public char verificaVincitore() { // controlla tutta la griglia per capire chi è il vincitore
         for (int i = 0; i < grandezzaGriglia; i++) {
-            if (grigliaPrincipale[i][0] == grigliaPrincipale[i][1] && grigliaPrincipale[i][1] == grigliaPrincipale[i][2] && grigliaPrincipale[i][0] != VUOTO) {
-                return grigliaPrincipale[i][0]; //returna X o O a seconda del carattere in quella posizione che abbiamo controllato
+            if (grigliaPrincipale[i][0] == grigliaPrincipale[i][1] && grigliaPrincipale[i][1] == grigliaPrincipale[i][2]
+                    && grigliaPrincipale[i][0] != VUOTO) {
+                return grigliaPrincipale[i][0]; // returna X o O a seconda del carattere in quella posizione che abbiamo
+                                                // controllato
             }
-            if (grigliaPrincipale[0][i] == grigliaPrincipale[1][i] && grigliaPrincipale[1][i] == grigliaPrincipale[2][i] && grigliaPrincipale[0][i] != VUOTO) {
+            if (grigliaPrincipale[0][i] == grigliaPrincipale[1][i] && grigliaPrincipale[1][i] == grigliaPrincipale[2][i]
+                    && grigliaPrincipale[0][i] != VUOTO) {
                 return grigliaPrincipale[0][i];
             }
         }
-        //controlla le linee in diagonale 
-        if (grigliaPrincipale[0][0] == grigliaPrincipale[1][1] && grigliaPrincipale[1][1] == grigliaPrincipale[2][2] && grigliaPrincipale[0][0] != VUOTO) {
+        // controlla le linee in diagonale
+        if (grigliaPrincipale[0][0] == grigliaPrincipale[1][1] && grigliaPrincipale[1][1] == grigliaPrincipale[2][2]
+                && grigliaPrincipale[0][0] != VUOTO) {
             return grigliaPrincipale[0][0];
         }
-        if (grigliaPrincipale[0][2] == grigliaPrincipale[1][1] && grigliaPrincipale[1][1] == grigliaPrincipale[2][0] && grigliaPrincipale[0][2] != VUOTO) {
+        if (grigliaPrincipale[0][2] == grigliaPrincipale[1][1] && grigliaPrincipale[1][1] == grigliaPrincipale[2][0]
+                && grigliaPrincipale[0][2] != VUOTO) {
             return grigliaPrincipale[0][2];
         }
 
-
-        return VUOTO; //pareggio 
+        return VUOTO; // pareggio
     }
-
 
     public boolean isPieno() {
         for (int i = 0; i < grandezzaGriglia; i++) {
             for (int j = 0; j < grandezzaGriglia; j++) {
                 if (grigliaPrincipale[i][j] == VUOTO) {
-                    return false; //ancora la griglia non è piena 
+                    return false; // ancora la griglia non è piena
                 }
             }
         }
-        return true; //la griglia è piena
+        return true; // la griglia è piena
     }
 
-
-    public void reset() {  //resetta tutta la griglia con il carattere vuoto
+    public void reset() { // resetta tutta la griglia con il carattere vuoto
         for (int i = 0; i < grandezzaGriglia; i++) {
             for (int j = 0; j < grandezzaGriglia; j++) {
                 grigliaPrincipale[i][j] = VUOTO;
             }
         }
-        turnoGiocatore1 = true;  //ripartiamo con il primo giocatore
+        turnoGiocatore1 = true; // ripartiamo con il primo giocatore
     }
 }
